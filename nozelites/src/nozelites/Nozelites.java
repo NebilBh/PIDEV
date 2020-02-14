511a3d348fa83fce90523e3438dc53bbf406cec4
Nebilbh <Nebil.Benhamouda@esprit.tn>
2020-02-14 17:16:57 +0100

    creation projet

---
+++ nozelites/src/nozelites/Nozelites.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nozelites;
+
+import Utilities.ConnexionDB;
+import java.sql.Connection;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Nebil
+ */
+public class Nozelites extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+        
+        
+       
+        
+    }
+    
+}
