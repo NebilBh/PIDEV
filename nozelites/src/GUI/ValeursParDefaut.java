d870653a6f1666ea1ead21db55276b36b7fd8790
nadhir_bouhaouala <nadhir_bouhaouala@live.fr>
2020-02-17 18:06:04 +0100

    groupes interfaces + parametres par defaut

---
+++ nozelites/src/GUI/ValeursParDefaut.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+/**
+ *
+ * @author nadhir
+ */
+public interface ValeursParDefaut {
+    
+    int longueur = 740;
+    int hauteur = 500;
+    
+}
