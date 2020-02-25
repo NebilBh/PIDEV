fed78349397fce2b323c277962a0edd06d304a2e
firasbelhiba <firas.belhiba@esprit.tn>
2020-02-25 02:46:47 +0100

    gestion des publications + commentaires + favoris

--- nozelites/src/nozelites/Nozelites.java
+++ nozelites/src/nozelites/Nozelites.java
@@ -50,8 +50,8 @@ public class Nozelites extends Application {
 
 
 
-      Parent root = FXMLLoader.load(getClass().getResource("/GUI/ConnectionInterface.fxml"));
-        // Parent root = FXMLLoader.load(getClass().getResource("/GUI/AdminAcceuilInterface.fxml"));
+      //Parent root = FXMLLoader.load(getClass().getResource("/GUI/ConnectionInterface.fxml"));
+         Parent root = FXMLLoader.load(getClass().getResource("/GUI/AdminAcceuilInterface.fxml"));
 
 //Parent root = FXMLLoader.load(getClass().getResource("/GUI/ReclamationInterface.fxml"));
    // Parent root = FXMLLoader.load(getClass().getResource("/GUI/MembrePortfolioAjouter_interface.fxml"));
